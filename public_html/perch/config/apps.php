b43795519d3683bc4ac1c587a7b90ed9e7c8b82c
Rachel <rachel.andrew@edgeofmyseat.com>
2014-09-16 14:35:55 +0100

    updating to Perch 2.6.5

--- public_html/perch/config/apps.php
+++ public_html/perch/config/apps.php
@@ -1,5 +1,6 @@
 <?php
     include(PERCH_PATH.'/core/apps/content/runtime.php');
+    include(PERCH_PATH.'/core/apps/categories/runtime.php');
     include(PERCH_PATH.'/addons/apps/perch_events/runtime.php');
     include(PERCH_PATH.'/addons/apps/perch_blog/runtime.php');
     include(PERCH_PATH.'/addons/apps/perch_gallery/runtime.php');
